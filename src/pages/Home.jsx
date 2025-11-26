8f8ec11f50c2f938533de3b22106080d356aa9a0
Jahangir Hosain <jahangirraj31@gmail.com>
2025-11-27 02:33:30 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner';
+import WhyChoose from '../components/WhyChoose';
+
+const Home = () => {
+    return (
+        <>
+            <Banner />
+            <WhyChoose />
+        </>
+    )
+}
+
+export default Home;
